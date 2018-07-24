e36fccfb697c22a920650012c7ef2c93c0ccc660
ERIVELTONIFTO2016 <17277270+ERIVELTONIFTO2016@users.noreply.github.com>
2018-07-24 17:59:25 -0300

    commit teste

---
+++ SOSBeer/app/src/main/java/sosbeerdelivery/com/sosbeer/cadasdroUsuario.java
@@ -0,0 +1,13 @@
+package sosbeerdelivery.com.sosbeer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadasdroUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadasdro_usuario);
+    }
+}
